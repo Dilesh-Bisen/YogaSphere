37befcc840b402480bbf0c7ab13150f2e3f944b7
Dilesh Bisen <2dileshbisen@gmail.com>
2025-02-07 01:17:19 +0530

    update-file

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config(); 
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3045;
 
-const uri = process.env.MONGODB_URI; 
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -18,6 +18,15 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
+
+        const database = client.db("yoga-guru");
+        const userCollection = database.collection("user");
+        const classCollection = database.collection("class");
+        const cartCollection = database.collection("cart");
+        const paymentCollection = database.collection("payment");
+        const enrolledCollection = database.collection("enrolled");
+        const appliedCollection = database.collection("applied");
+
         await client.db("admin").command({ ping: 1 });
         console.log("You successfully connected to MongoDB!");
     } finally {
