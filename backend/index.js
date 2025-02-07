8327fe49f6bb45f74b6d82178f34df5732df089c
Dilesh Bisen <2dileshbisen@gmail.com>
2025-02-07 12:35:23 +0530

    update-database

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3045;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
 
@@ -27,16 +32,29 @@ async function run() {
         const enrolledCollection = database.collection("enrolled");
         const appliedCollection = database.collection("applied");
 
+        app.post('/new-class', async (req, res) => {
+            const newClass = req.body;
+            const result = await classCollection.insertOne(newClass);
+            res.send(result);
+        });
+
+        app.get('/classes', async (req, res) => {
+            const newClass = req.body;
+            const result = await classCollection.insertOne(newClass);
+            res.send(result);
+        });
+
         await client.db("admin").command({ ping: 1 });
         console.log("You successfully connected to MongoDB!");
-    } finally {
-        await client.close();
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
     }
 }
+
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello Dilesh!')
+    res.send('Hello Dilesh!');
 });
 
 app.listen(port, () => {
